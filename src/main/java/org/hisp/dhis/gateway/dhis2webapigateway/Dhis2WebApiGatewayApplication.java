80bdc98dc8b28ae8cb27e16ae497a0b7aec9f2f7
David Mackessy <eternalgooner@hotmail.com>
2023-01-20 17:08:31 +0000

    add init src files and update gitignore

---
+++ src/main/java/org/hisp/dhis/gateway/dhis2webapigateway/Dhis2WebApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.hisp.dhis.gateway.dhis2webapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dhis2WebApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dhis2WebApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hisp/dhis/gateway/dhis2webapigateway/Dhis2WebApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hisp.dhis.gateway.dhis2webapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dhis2WebApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
